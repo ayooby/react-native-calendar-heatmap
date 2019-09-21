fcca2959b13bb8461280c2463c8eca2afada7a04
FreakyCoder <kurayogun@gmail.com>
2019-09-21 22:57:48 +0300

    Finally refactored library is working

---
+++ example/lib/src/utils/constants.js
@@ -0,0 +1,33 @@
+const SQUARE_SIZE = 20;
+const MONTH_LABEL_GUTTER_SIZE = 8;
+const MILLISECONDS_IN_ONE_DAY = 24 * 60 * 60 * 1000;
+const DAYS_IN_WEEK = 7;
+const MONTH_LABELS = {
+  0: "Jan",
+  1: "Feb",
+  2: "Mar",
+  3: "Apr",
+  4: "May",
+  5: "Jun",
+  6: "Jul",
+  7: "Aug",
+  8: "Sep",
+  9: "Oct",
+  10: "Nov",
+  11: "Dec"
+};
+
+export {
+  SQUARE_SIZE,
+  MONTH_LABELS,
+  DAYS_IN_WEEK,
+  MONTH_LABEL_GUTTER_SIZE,
+  MILLISECONDS_IN_ONE_DAY
+};
+export default {
+  SQUARE_SIZE,
+  MONTH_LABELS,
+  DAYS_IN_WEEK,
+  MONTH_LABEL_GUTTER_SIZE,
+  MILLISECONDS_IN_ONE_DAY
+};
---
+++ example/lib/src/utils/utils.js
@@ -0,0 +1,191 @@
+import { shiftDate, getBeginningTimeForDate, convertToDate } from "./helpers";
+import {
+  SQUARE_SIZE,
+  MONTH_LABELS,
+  DAYS_IN_WEEK,
+  MONTH_LABEL_GUTTER_SIZE,
+  MILLISECONDS_IN_ONE_DAY
+} from "./constants";
+
+function getTransformForMonthLabels(horizontal, gutterSize) {
+  if (horizontal) {
+    return null;
+  }
+  return `${getWeekWidth(gutterSize) + MONTH_LABEL_GUTTER_SIZE}, 0`;
+}
+
+function getTransformForAllWeeks(showMonthLabels, horizontal) {
+  if (horizontal)
+    return `0, ${getMonthLabelSize(showMonthLabels, horizontal) - 100}`;
+  return null;
+}
+
+function getWeekWidth(gutterSize) {
+  console.log("getSquareSizeWithGutter: ", getSquareSizeWithGutter(gutterSize));
+  return DAYS_IN_WEEK * getSquareSizeWithGutter(gutterSize);
+}
+
+function getWidth(numDays, endDate, gutterSize) {
+  return (
+    getWeekCount(numDays, endDate) * getSquareSizeWithGutter(gutterSize) -
+    gutterSize
+  );
+}
+
+function getHeight(gutterSize, showMonthLabels, horizontal) {
+  return (
+    getWeekWidth(gutterSize) +
+    (getMonthLabelSize(showMonthLabels, horizontal) - gutterSize)
+  );
+}
+
+function getViewBox(numDays, endDate, gutterSize, showMonthLabels, horizontal) {
+  if (horizontal) {
+    return `${getWidth(numDays, endDate, gutterSize)} ${getHeight(
+      gutterSize,
+      showMonthLabels,
+      horizontal
+    )}`;
+  }
+  return `${getHeight(gutterSize, showMonthLabels, horizontal)} ${getWidth(
+    numDays,
+    endDate,
+    gutterSize
+  )}`;
+}
+
+function getValueForIndex(index, valueCache) {
+  if (valueCache[index]) return valueCache[index].value;
+  return null;
+}
+
+function getTooltipDataAttrsForValue(value, tooltipDataAttrs) {
+  if (typeof tooltipDataAttrs === "function") return tooltipDataAttrs(value);
+  return tooltipDataAttrs;
+}
+
+function getTooltipDataAttrsForIndex(index, valueCache, tooltipDataAttrs) {
+  if (valueCache[index]) {
+    return valueCache[index].tooltipDataAttrs;
+  }
+  return getTooltipDataAttrsForValue(
+    { date: null, count: null },
+    tooltipDataAttrs
+  );
+}
+
+function getClassNameForIndex(index, valueCache, rectColor) {
+  if (valueCache[index]) return rectColor[valueCache[index].value.count];
+  return rectColor[0];
+}
+
+function getTitleForIndex(index, valueCache, titleForValue) {
+  console.log("valueCache[index]", valueCache[index]);
+  if (valueCache[index]) return valueCache[index].title;
+  return titleForValue ? titleForValue(null) : null;
+}
+
+function getSquareCoordinates(dayIndex, horizontal, gutterSize) {
+  if (horizontal) return [0, dayIndex * getSquareSizeWithGutter(gutterSize)];
+  return [dayIndex * getSquareSizeWithGutter(gutterSize), 0];
+}
+
+function getTransformForWeek(weekIndex, horizontal, gutterSize) {
+  if (horizontal) {
+    return [weekIndex * getSquareSizeWithGutter(gutterSize), 50];
+  }
+  return [10, weekIndex * getSquareSizeWithGutter(gutterSize)];
+}
+
+function getMonthLabelSize(showMonthLabels, horizontal) {
+  if (!showMonthLabels) {
+    return 0;
+  } else if (horizontal) {
+    return SQUARE_SIZE + MONTH_LABEL_GUTTER_SIZE;
+  }
+  return 2 * (SQUARE_SIZE + MONTH_LABEL_GUTTER_SIZE);
+}
+
+function getSquareSizeWithGutter(gutterSize) {
+  return SQUARE_SIZE + gutterSize;
+}
+
+function getMonthLabelCoordinates(
+  weekIndex,
+  horizontal,
+  gutterSize,
+  showMonthLabels
+) {
+  if (horizontal) {
+    return [
+      weekIndex * getSquareSizeWithGutter(gutterSize),
+      getMonthLabelSize(showMonthLabels, horizontal) - MONTH_LABEL_GUTTER_SIZE
+    ];
+  }
+  const verticalOffset = -2;
+  return [
+    0,
+    (weekIndex + 1) * getSquareSizeWithGutter(gutterSize) + verticalOffset
+  ];
+}
+
+function getStartDateWithEmptyDays(numDays, endDate) {
+  return shiftDate(
+    getStartDate(numDays, endDate),
+    -getNumEmptyDaysAtStart(numDays, endDate)
+  );
+}
+
+function getEndDate(endDate) {
+  return getBeginningTimeForDate(convertToDate(endDate));
+}
+
+function getStartDate(numDays, endDate) {
+  return shiftDate(getEndDate(endDate), -numDays + 1); // +1 because endDate is inclusive
+}
+
+function getNumEmptyDaysAtEnd(endDate) {
+  return DAYS_IN_WEEK - 1 - getEndDate(endDate).getDay();
+}
+
+function getNumEmptyDaysAtStart(numDays, endDate) {
+  return getStartDate(numDays, endDate).getDay();
+}
+
+function getWeekCount(numDays, endDate) {
+  const numDaysRoundedToWeek =
+    numDays +
+    getNumEmptyDaysAtStart(numDays, endDate) +
+    getNumEmptyDaysAtEnd(endDate);
+  return Math.ceil(numDaysRoundedToWeek / DAYS_IN_WEEK);
+}
+
+export {
+  getWeekCount,
+  getStartDateWithEmptyDays,
+  getMonthLabelCoordinates,
+  getTransformForWeek,
+  getNumEmptyDaysAtStart,
+  getSquareCoordinates,
+  getTitleForIndex,
+  getClassNameForIndex,
+  getTooltipDataAttrsForIndex,
+  getTooltipDataAttrsForValue,
+  getHeight,
+  getWidth
+};
+
+export default {
+  getWeekCount,
+  getStartDateWithEmptyDays,
+  getMonthLabelCoordinates,
+  getTransformForWeek,
+  getNumEmptyDaysAtStart,
+  getSquareCoordinates,
+  getTitleForIndex,
+  getClassNameForIndex,
+  getTooltipDataAttrsForIndex,
+  getTooltipDataAttrsForValue,
+  getHeight,
+  getWidth
+};
