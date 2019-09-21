57d6cd87ef73cd2956c35ceaefa3844974b3b337
FreakyCoder <kurayogun@gmail.com>
2019-09-22 01:03:02 +0300

    Example is updated

--- example/App.js
+++ example/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { View, Text, StatusBar, SafeAreaView } from "react-native";
-import CalendarHeatmap from "./lib/src/CalendarHeatmap";
+import CalendarHeatmap from "react-native-calender-heatmap";
 import { staticData } from "./staticData/staticData";
 
 const App = () => {
