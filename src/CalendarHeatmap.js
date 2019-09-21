42033f997abe9e4ddce2b9211d4a1b5e30f9460f
FreakyCoder <kurayogun@gmail.com>
2019-09-21 16:35:07 +0300

    New architecture

---
+++ src/CalendarHeatmap.js
@@ -0,0 +1,166 @@
+import _ from "lodash";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { ScrollView } from "react-native";
+import Svg, { G, Rect, Text } from "react-native-svg";
+import {
+  SQUARE_SIZE,
+  MONTH_LABELS,
+  DAYS_IN_WEEK,
+  MONTH_LABEL_GUTTER_SIZE,
+  MILLISECONDS_IN_ONE_DAY
+} from "./utils/constants";
+import {
+  shiftDate,
+  getBeginningTimeForDate,
+  convertToDate
+} from "./utils/helpers";
+import {
+  getWeekCount,
+  getStartDateWithEmptyDays,
+  getMonthLabelCoordinates,
+  getTransformForWeek,
+  getNumEmptyDaysAtStart,
+  getSquareCoordinates,
+  getTitleForIndex,
+  getClassNameForIndex,
+  getTooltipDataAttrsForIndex
+} from "./utils/utils";
+
+const rectColor = ["#eeeeee", "#d6e685", "#8cc665", "#44a340", "#1e6823"];
+
+const CalendarHeatmap = props => {
+  const {
+    gutterSize,
+    showMonthLabels,
+    horizontal,
+    numDays,
+    endDate,
+    titleForValue,
+    tooltipDataAttrs,
+    onPress,
+    showOutOfRangeDays,
+    showMonthLabels
+  } = props;
+  const [valueCache, setValueCache] = useState(null);
+
+  handleClick = value => {
+    if (onPress) {
+      onPress(value);
+    }
+  };
+
+  renderSquare = (dayIndex, index) => {
+    const indexOutOfRange =
+      index < getNumEmptyDaysAtStart(numDays, endDate) ||
+      index >= getNumEmptyDaysAtStart(numDays, endDate) + numDays;
+    if (indexOutOfRange && !showOutOfRangeDays) {
+      return null;
+    }
+    const [x, y] = getSquareCoordinates(dayIndex, horizontal, gutterSize);
+    return (
+      <Rect
+        key={index}
+        width={SQUARE_SIZE}
+        height={SQUARE_SIZE}
+        x={x}
+        y={y}
+        title={getTitleForIndex(index, valueCache, titleForValue)}
+        onPress={() => handleClick(index)}
+        fill={getClassNameForIndex(index, valueCache, rectColor)}
+        {...getTooltipDataAttrsForIndex(index, valueCache, tooltipDataAttrs)}
+      />
+    );
+  };
+
+  renderWeek = weekIndex => {
+    const [x, y] = getTransformForWeek(weekIndex, horizontal, gutterSize);
+    return (
+      <G key={weekIndex} x={x} y={y}>
+        {_.range(DAYS_IN_WEEK).map(dayIndex =>
+          renderSquare(dayIndex, weekIndex * DAYS_IN_WEEK + dayIndex)
+        )}
+      </G>
+    );
+  };
+
+  renderAllWeeks = () => {
+    return _.range(getWeekCount(numDays, endDate)).map(weekIndex =>
+      renderWeek(weekIndex)
+    );
+  };
+
+  renderMonthLabels = () => {
+    if (!showMonthLabels) {
+      return null;
+    }
+    const weekRange = _.range(getWeekCount(numDays, endDate) - 1); // don't render for last week, because label will be cut off
+    return weekRange.map(weekIndex => {
+      const endOfWeek = shiftDate(
+        getStartDateWithEmptyDays(numDays, endDate),
+        (weekIndex + 1) * DAYS_IN_WEEK
+      );
+      const [x, y] = getMonthLabelCoordinates(
+        weekIndex,
+        horizontal,
+        gutterSize,
+        showMonthLabels,
+        horizontal
+      );
+      return endOfWeek.getDate() >= 1 && endOfWeek.getDate() <= DAYS_IN_WEEK ? (
+        <Text key={weekIndex} x={x} y={y}>
+          {MONTH_LABELS[endOfWeek.getMonth()]}
+        </Text>
+      ) : null;
+    });
+  };
+
+  return (
+    <ScrollView>
+      <Svg height={this.getHeight()} width={this.getWidth()}>
+        <G>{this.renderMonthLabels()}</G>
+        <G>{this.renderAllWeeks()}</G>
+      </Svg>
+    </ScrollView>
+  );
+};
+
+CalendarHeatmap.propTypes = {
+  values: PropTypes.arrayOf(
+    // array of objects with date and arbitrary metadata
+    PropTypes.shape({
+      date: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.instanceOf(Date)
+      ]).isRequired
+    }).isRequired
+  ).isRequired,
+  numDays: PropTypes.number, // number of days back from endDate to show
+  endDate: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf(Date)
+  ]), // end of date range
+  gutterSize: PropTypes.number, // size of space between squares
+  horizontal: PropTypes.bool, // whether to orient horizontally or vertically
+  showMonthLabels: PropTypes.bool, // whether to show month labels
+  showOutOfRangeDays: PropTypes.bool, // whether to render squares for extra days in week after endDate, and before start date
+  tooltipDataAttrs: PropTypes.oneOfType([PropTypes.object, PropTypes.func]), // data attributes to add to square for setting 3rd party tooltips, e.g. { 'data-toggle': 'tooltip' } for bootstrap tooltips
+  titleForValue: PropTypes.func, // function which returns title text for value
+  classForValue: PropTypes.func, // function which returns html class for value
+  onPress: PropTypes.func // callback function when a square is clicked
+};
+
+CalendarHeatmap.defaultProps = {
+  numDays: 200,
+  endDate: new Date(),
+  gutterSize: 1,
+  horizontal: true,
+  showMonthLabels: true,
+  showOutOfRangeDays: false,
+  classForValue: value => (value ? "black" : "#8cc665"),
+  onPress: () => console.log("change onPress prop")
+};
+
+export default CalendarHeatmap;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
-import ViewPropTypes from 'react-native';
-import PropTypes from 'prop-types';
-import { ScrollView } from 'react-native';
-import Svg,{
-    G,
-    Rect,
-    Text
-} from 'react-native-svg';
-import _ from 'lodash';
+import React, { Component } from "react";
+import ViewPropTypes from "react-native";
+import PropTypes from "prop-types";
+import { ScrollView } from "react-native";
+import Svg, { G, Rect, Text } from "react-native-svg";
+import _ from "lodash";
+import {
+  SQUARE_SIZE,
+  MONTH_LABELS,
+  DAYS_IN_WEEK,
+  MONTH_LABEL_GUTTER_SIZE,
+  MILLISECONDS_IN_ONE_DAY
+} from "./utils/constants";
+import {
+  shiftDate,
+  getBeginningTimeForDate,
+  convertToDate
+} from "./utils/helpers";
 
-import { DAYS_IN_WEEK, MILLISECONDS_IN_ONE_DAY, MONTH_LABELS } from './constants';
-import { shiftDate, getBeginningTimeForDate, convertToDate } from './dateHelpers';
-
-const SQUARE_SIZE = 20;
-const MONTH_LABEL_GUTTER_SIZE = 8;
-const rectColor = ['#eeeeee','#d6e685', '#8cc665', '#44a340', '#1e6823']
+const rectColor = ["#eeeeee", "#d6e685", "#8cc665", "#44a340", "#1e6823"];
 
 export default class CalendarHeatmap extends Component<Props> {
   constructor(props) {
     super(props);
     this.state = {
-      valueCache: this.getValueCache(props.values),
+      valueCache: this.getValueCache(props.values)
     };
   }
   getSquareSizeWithGutter() {
@@ -53,38 +56,54 @@ export default class CalendarHeatmap extends Component<Props> {
   }
 
   getNumEmptyDaysAtEnd() {
-    return (DAYS_IN_WEEK - 1) - this.getEndDate().getDay();
+    return DAYS_IN_WEEK - 1 - this.getEndDate().getDay();
   }
 
   getWeekCount() {
-    const numDaysRoundedToWeek = this.props.numDays + this.getNumEmptyDaysAtStart() + this.getNumEmptyDaysAtEnd();
+    const numDaysRoundedToWeek =
+      this.props.numDays +
+      this.getNumEmptyDaysAtStart() +
+      this.getNumEmptyDaysAtEnd();
     return Math.ceil(numDaysRoundedToWeek / DAYS_IN_WEEK);
   }
 
   getWeekWidth() {
-  console.log(this.getSquareSizeWithGutter());
+    console.log(this.getSquareSizeWithGutter());
     return DAYS_IN_WEEK * this.getSquareSizeWithGutter();
   }
 
   getWidth() {
-    return (this.getWeekCount() * this.getSquareSizeWithGutter()) - this.props.gutterSize;
+    return (
+      this.getWeekCount() * this.getSquareSizeWithGutter() -
+      this.props.gutterSize
+    );
   }
 
   getHeight() {
-    return this.getWeekWidth() + (this.getMonthLabelSize() - this.props.gutterSize);
+    return (
+      this.getWeekWidth() + (this.getMonthLabelSize() - this.props.gutterSize)
+    );
   }
 
   getValueCache(values) {
-    return _.reduce(values, (memo, value) => {
-      const date = convertToDate(value.date);
-      const index = Math.floor((date - this.getStartDateWithEmptyDays()) / MILLISECONDS_IN_ONE_DAY);
-      memo[index] = {
-        value,
-        title: this.props.titleForValue ? this.props.titleForValue(value) : null,
-        tooltipDataAttrs: this.getTooltipDataAttrsForValue(value),
-      };
-      return memo;
-    }, {});
+    return _.reduce(
+      values,
+      (memo, value) => {
+        const date = convertToDate(value.date);
+        const index = Math.floor(
+          (date - this.getStartDateWithEmptyDays()) / MILLISECONDS_IN_ONE_DAY
+        );
+        memo[index] = {
+          value,
+          title: this.props.titleForValue
+            ? this.props.titleForValue(value)
+            : null,
+          tooltipDataAttrs: this.getTooltipDataAttrsForValue(value)
+        };
+        return memo;
+      },
+      {}
+    );
   }
 
   getValueForIndex(index) {
@@ -118,7 +137,7 @@ export default class CalendarHeatmap extends Component<Props> {
   getTooltipDataAttrsForValue(value) {
     const { tooltipDataAttrs } = this.props;
 
-    if (typeof tooltipDataAttrs === 'function') {
+    if (typeof tooltipDataAttrs === "function") {
       return tooltipDataAttrs(value);
     }
     return tooltipDataAttrs;
@@ -163,13 +182,13 @@ export default class CalendarHeatmap extends Component<Props> {
     if (this.props.horizontal) {
       return [
         weekIndex * this.getSquareSizeWithGutter(),
-        this.getMonthLabelSize() - MONTH_LABEL_GUTTER_SIZE,
+        this.getMonthLabelSize() - MONTH_LABEL_GUTTER_SIZE
       ];
     }
     const verticalOffset = -2;
     return [
       0,
-      ((weekIndex + 1) * this.getSquareSizeWithGutter()) + verticalOffset,
+      (weekIndex + 1) * this.getSquareSizeWithGutter() + verticalOffset
     ];
   }
 
@@ -180,7 +199,9 @@ export default class CalendarHeatmap extends Component<Props> {
   }
 
   renderSquare(dayIndex, index) {
-    const indexOutOfRange = index < this.getNumEmptyDaysAtStart() || index >= this.getNumEmptyDaysAtStart() + this.props.numDays;
+    const indexOutOfRange =
+      index < this.getNumEmptyDaysAtStart() ||
+      index >= this.getNumEmptyDaysAtStart() + this.props.numDays;
     if (indexOutOfRange && !this.props.showOutOfRangeDays) {
       return null;
     }
@@ -204,29 +225,32 @@ export default class CalendarHeatmap extends Component<Props> {
     const [x, y] = this.getTransformForWeek(weekIndex);
     return (
       <G key={weekIndex} x={x} y={y}>
-        {_.range(DAYS_IN_WEEK).map(dayIndex => this.renderSquare(dayIndex, (weekIndex * DAYS_IN_WEEK) + dayIndex))}
+        {_.range(DAYS_IN_WEEK).map(dayIndex =>
+          this.renderSquare(dayIndex, weekIndex * DAYS_IN_WEEK + dayIndex)
+        )}
       </G>
     );
   }
 
   renderAllWeeks() {
-    return _.range(this.getWeekCount()).map(weekIndex => this.renderWeek(weekIndex));
+    return _.range(this.getWeekCount()).map(weekIndex =>
+      this.renderWeek(weekIndex)
+    );
   }
 
   renderMonthLabels() {
     if (!this.props.showMonthLabels) {
       return null;
     }
-    const weekRange = _.range(this.getWeekCount() - 1);  // don't render for last week, because label will be cut off
-    return weekRange.map((weekIndex) => {
-      const endOfWeek = shiftDate(this.getStartDateWithEmptyDays(), (weekIndex + 1) * DAYS_IN_WEEK);
+    const weekRange = _.range(this.getWeekCount() - 1); // don't render for last week, because label will be cut off
+    return weekRange.map(weekIndex => {
+      const endOfWeek = shiftDate(
+        this.getStartDateWithEmptyDays(),
+        (weekIndex + 1) * DAYS_IN_WEEK
+      );
       const [x, y] = this.getMonthLabelCoordinates(weekIndex);
-      return (endOfWeek.getDate() >= 1 && endOfWeek.getDate() <= DAYS_IN_WEEK) ? (
-        <Text
-          key={weekIndex}
-          x={x}
-          y={y}
-        >
+      return endOfWeek.getDate() >= 1 && endOfWeek.getDate() <= DAYS_IN_WEEK ? (
+        <Text key={weekIndex} x={x} y={y}>
           {MONTH_LABELS[endOfWeek.getMonth()]}
         </Text>
       ) : null;
@@ -236,16 +260,9 @@ export default class CalendarHeatmap extends Component<Props> {
   render() {
     return (
       <ScrollView>
-        <Svg
-          height={this.getHeight()} 
-          width={this.getWidth()}
-        >
-          <G>
-            {this.renderMonthLabels()}
-          </G>
-          <G >
-            {this.renderAllWeeks()}
-          </G>
+        <Svg height={this.getHeight()} width={this.getWidth()}>
+          <G>{this.renderMonthLabels()}</G>
+          <G>{this.renderAllWeeks()}</G>
         </Svg>
       </ScrollView>
     );
@@ -253,21 +270,30 @@ export default class CalendarHeatmap extends Component<Props> {
 }
 
 CalendarHeatmap.ViewPropTypes = {
-  values: PropTypes.arrayOf(             // array of objects with date and arbitrary metadata
+  values: PropTypes.arrayOf(
+    // array of objects with date and arbitrary metadata
     PropTypes.shape({
-      date: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.instanceOf(Date)]).isRequired,
+      date: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.instanceOf(Date)
+      ]).isRequired
     }).isRequired
   ).isRequired,
-  numDays: PropTypes.number,             // number of days back from endDate to show
-  endDate: PropTypes.oneOfType([PropTypes.string, PropTypes.number, PropTypes.instanceOf(Date)]),  // end of date range
-  gutterSize: PropTypes.number,          // size of space between squares
-  horizontal: PropTypes.bool,            // whether to orient horizontally or vertically
-  showMonthLabels: PropTypes.bool,       // whether to show month labels
-  showOutOfRangeDays: PropTypes.bool,    // whether to render squares for extra days in week after endDate, and before start date
-  tooltipDataAttrs: PropTypes.oneOfType([PropTypes.object, PropTypes.func]),    // data attributes to add to square for setting 3rd party tooltips, e.g. { 'data-toggle': 'tooltip' } for bootstrap tooltips
-  titleForValue: PropTypes.func,         // function which returns title text for value
-  classForValue: PropTypes.func,         // function which returns html class for value
-  onPress: PropTypes.func,               // callback function when a square is clicked
+  numDays: PropTypes.number, // number of days back from endDate to show
+  endDate: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf(Date)
+  ]), // end of date range
+  gutterSize: PropTypes.number, // size of space between squares
+  horizontal: PropTypes.bool, // whether to orient horizontally or vertically
+  showMonthLabels: PropTypes.bool, // whether to show month labels
+  showOutOfRangeDays: PropTypes.bool, // whether to render squares for extra days in week after endDate, and before start date
+  tooltipDataAttrs: PropTypes.oneOfType([PropTypes.object, PropTypes.func]), // data attributes to add to square for setting 3rd party tooltips, e.g. { 'data-toggle': 'tooltip' } for bootstrap tooltips
+  titleForValue: PropTypes.func, // function which returns title text for value
+  classForValue: PropTypes.func, // function which returns html class for value
+  onPress: PropTypes.func // callback function when a square is clicked
 };
 
 CalendarHeatmap.defaultProps = {
@@ -277,7 +303,6 @@ CalendarHeatmap.defaultProps = {
   horizontal: true,
   showMonthLabels: true,
   showOutOfRangeDays: false,
-  classForValue: value => (value ? 'black' : '#8cc665'),
-  onPress: () => console.log('change onPress prop')
+  classForValue: value => (value ? "black" : "#8cc665"),
+  onPress: () => console.log("change onPress prop")
 };
-
