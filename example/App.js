39ac83d657ccf2251aed25afd6b8ea9dcf34b4a2
Joberth Rogers <joberth.rogers18@gmail.com>
2022-03-19 17:32:03 -0400

    feat: add label hours and change month to day

--- example/App.js
+++ example/App.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { View, Text, StatusBar, SafeAreaView } from "react-native";
-import CalendarHeatmap from "@freakycoder/react-native-calendar-heatmap";
+// import CalendarHeatmap from "@freakycoder/react-native-calendar-heatmap";
+import CalendarHeatmap from './CalendarHeatmap';
 import { staticData } from "./staticData/staticData";
 
 const App = () => {
@@ -10,34 +11,10 @@ const App = () => {
       <SafeAreaView style={{ flex: 1 }}>
         <View style={{ flex: 1 }}>
           <View style={{ margin: 16 }}>
-            <Text style={{ fontWeight: "600", fontSize: 16, color: "#D44B79" }}>
-              Red One
-            </Text>
             <CalendarHeatmap
-              endDate={new Date("2019-03-25")}
-              numDays={100}
+              startDate={new Date("2019-01-01")}
+              endDate={new Date("2019-02-05")}
               colorArray={["#eee", "#D44B79", "#6B1928", "#9F3251", "#360000"]}
-              values={staticData}
-            />
-          </View>
-          <View style={{ margin: 16 }}>
-            <Text style={{ fontWeight: "600", fontSize: 16, color: "#656ac6" }}>
-              Blue One
-            </Text>
-            <CalendarHeatmap
-              endDate={new Date("2019-03-25")}
-              numDays={100}
-              colorArray={["#eee", "#bcd6f7", "#656ac6", "#393b99", "#191c5c"]}
-              values={staticData}
-            />
-          </View>
-          <View style={{ margin: 16 }}>
-            <Text style={{ fontWeight: "600", fontSize: 16, color: "#8cc665" }}>
-              Default
-            </Text>
-            <CalendarHeatmap
-              endDate={new Date("2019-03-25")}
-              numDays={100}
               values={staticData}
             />
           </View>
---
+++ example/CalendarHeatmap.js
@@ -0,0 +1,284 @@
+import _ from "lodash";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { ScrollView } from "react-native";
+import Svg, { G, Rect, Text } from "react-native-svg";
+import {
+  SQUARE_SIZE,
+  MONTH_LABELS,
+  DAYS_IN_WEEK,
+  MONTH_LABEL_GUTTER_SIZE,
+  MILLISECONDS_IN_ONE_DAY
+} from "./utils/constants";
+import {
+  shiftDate,
+  getBeginningTimeForDate,
+  convertToDate
+} from "./utils/helpers";
+import {
+  getWeekCount,
+  getStartDateWithEmptyDays,
+  getMonthLabelCoordinates,
+  getTransformForWeek,
+  getNumEmptyDaysAtStart,
+  getSquareCoordinates,
+  getTitleForIndex,
+  getFillColor,
+  getCountByDuplicateValues,
+  getTooltipDataAttrsForIndex,
+  getTooltipDataAttrsForValue,
+  getHeight,
+  getWidth,
+  getDateCount,
+  getLabelDay
+} from "./utils/utils";
+
+const rectColor = ["#eeeeee", "#d6e685", "#8cc665", "#44a340", "#1e6823"];
+
+const CalendarHeatmap = props => {
+  const {
+    values,
+    gutterSize,
+    horizontal,
+    numDays,
+    endDate,
+    startDate,
+    titleForValue,
+    tooltipDataAttrs,
+    onPress,
+    showOutOfRangeDays,
+    monthLabelsColor,
+    showMonthLabels,
+    monthLabelsStyle,
+    monthLabelForIndex,
+    colorArray,
+  } = props;
+
+  getValueCache = values => {
+    const countedArray = getCountByDuplicateValues(values);
+    return _.reduce(
+      values,
+      (memo, value) => {
+        const date = convertToDate(value.date);
+        const index = Math.floor(
+          (date - getStartDateWithEmptyDays(numDays, endDate)) /
+            MILLISECONDS_IN_ONE_DAY
+        );
+        memo[index] = {
+          value: value
+        };
+        if (memo[index].value.count) {
+          memo[index].countedArray = memo[index].value;
+        } else {
+          const count = _.find(countedArray, { key: memo[index].value.date });
+          memo[index].countedArray = count;
+        }
+
+        return memo;
+      },
+      {}
+    );
+  };
+
+  useEffect(() => {
+    setValueCache(getValueCache(values));
+  }, [values]);
+
+  const [valueCache, setValueCache] = useState(getValueCache(values));
+
+  handleClick = value => {
+    if (onPress) {
+      onPress(value);
+    }
+  };
+
+  renderSquare = (dayIndex, index) => {
+    const indexOutOfRange =
+      index < getNumEmptyDaysAtStart(numDays, endDate) ||
+      index >= getNumEmptyDaysAtStart(numDays, endDate) + numDays;
+    // if (indexOutOfRange && !showOutOfRangeDays) {
+    //   return null;
+    // }
+    const [x, y] = getSquareCoordinates(dayIndex, horizontal, gutterSize);
+    const fillColor = getFillColor(index, valueCache, colorArray);
+    return (
+      <Rect
+        key={index}
+        width={SQUARE_SIZE}
+        height={SQUARE_SIZE}
+        x={x + 25}
+        y={y + 12}
+        title={getTitleForIndex(index, valueCache, titleForValue)}
+        onPress={() => handleClick(index)}
+        fill={fillColor}
+        {...getTooltipDataAttrsForIndex(index, valueCache, tooltipDataAttrs)}
+      />
+    );
+  };
+
+  renderWeek = weekIndex => {
+    const [x, y] = getTransformForWeek(weekIndex, horizontal, gutterSize, showMonthLabels);
+    return (
+      <G key={weekIndex} x={x} y={y}>
+        {_.range(DAYS_IN_WEEK).map(dayIndex =>
+          renderSquare(dayIndex, weekIndex * DAYS_IN_WEEK + dayIndex)
+        )}
+      </G>
+    );
+  };
+
+  renderAllWeeks = () => {
+    return _.range(getDateCount(startDate, endDate)).map(weekIndex =>
+      renderWeek(weekIndex)
+    );
+  };
+
+  // renderDaysLabels = () => {
+  //   if (!showMonthLabels) {
+  //     return null;
+  //   }
+  //   const weekRange = _.range(getWeekCount(numDays, endDate) - 1); // don't render for last week, because label will be cut off
+  //   return weekRange.map(weekIndex => {
+  //     console.log('week index', weekIndex);
+  //     const endOfWeek = shiftDate(
+  //       getStartDateWithEmptyDays(numDays, endDate),
+  //       (weekIndex + 1) * DAYS_IN_WEEK
+  //     );
+  //     const [x, y] = getMonthLabelCoordinates(
+  //       weekIndex,
+  //       horizontal,
+  //       gutterSize,
+  //     );
+  //     // return endOfWeek.getDate() >= 1 && endOfWeek.getDate() <= DAYS_IN_WEEK ? (
+  //     return weekIndex % 3 === 0 ? (
+  //       <Text
+  //         {...monthLabelsStyle}
+  //         key={weekIndex}
+  //         x={x}
+  //         y={y + 16}
+  //         stroke={monthLabelsColor}
+  //       >
+  //         {monthLabelForIndex
+  //           ? monthLabelForIndex(endOfWeek.getMonth())
+  //           : MONTH_LABELS[endOfWeek.getMonth()]}
+  //       </Text>
+  //     ) : null;
+  //   });
+  // };
+
+  renderDaysLabels = () => {
+    if (!showMonthLabels) {
+      return null;
+    }
+    const dayRange = _.range(getDateCount(startDate, endDate)); 
+    return dayRange.map(weekIndex => {
+      const [x, y] = getMonthLabelCoordinates(
+        weekIndex,
+        horizontal,
+        gutterSize,
+      );
+      // return endOfWeek.getDate() >= 1 && endOfWeek.getDate() <= DAYS_IN_WEEK ? (
+      return weekIndex % 5 === 0 ? (
+        <Text
+          {...monthLabelsStyle}
+          key={weekIndex}
+          x={x + 22}
+          y={y + 16}
+          stroke={monthLabelsColor}
+        >
+          {/* {monthLabelForIndex
+            ? monthLabelForIndex(endOfWeek.getMonth())
+            : MONTH_LABELS[endOfWeek.getMonth()]} */}
+          {getLabelDay(startDate, weekIndex)}
+        </Text>
+      ) : null;
+    });
+  };
+
+  renderHourLabels = () => {
+    if (!showMonthLabels) {
+      return null;
+    }
+    const hourRange = _.range(DAYS_IN_WEEK); 
+    return hourRange.map(hourIndex => {
+      const [x, y] = getMonthLabelCoordinates(
+        hourIndex,
+        false,
+        gutterSize,
+      );
+      // return endOfWeek.getDate() >= 1 && endOfWeek.getDate() <= DAYS_IN_WEEK ? (
+      return hourIndex % 4 === 0 ? (
+        <Text
+          {...monthLabelsStyle}
+          key={hourIndex}
+          x={x}
+          y={y + 30}
+          stroke={monthLabelsColor}
+        >
+          {/* {monthLabelForIndex
+            ? monthLabelForIndex(endOfWeek.getMonth())
+            : MONTH_LABELS[endOfWeek.getMonth()]} */}
+          {`${hourIndex}h`}
+        </Text>
+      ) : null;
+    });
+  };
+
+  return (
+    <ScrollView horizontal={true}>
+      <Svg
+        height={getHeight(gutterSize + 2, showMonthLabels, horizontal)}
+        width={getWidth(startDate, endDate, gutterSize)}
+        style={{overflow:"scroll"}}
+      >
+        <G>{renderDaysLabels()}</G>
+        <G>{renderHourLabels()}</G>
+        <G>{renderAllWeeks()}</G>
+      </Svg>
+    </ScrollView>
+  );
+};
+
+CalendarHeatmap.propTypes = {
+  values: PropTypes.arrayOf(
+    // array of objects with date and arbitrary metadata
+    PropTypes.shape({
+      date: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.instanceOf(Date)
+      ]).isRequired
+    }).isRequired
+  ).isRequired,
+  numDays: PropTypes.number, // number of days back from endDate to show
+  endDate: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.instanceOf(Date)
+  ]), // end of date range
+  gutterSize: PropTypes.number, // size of space between squares
+  horizontal: PropTypes.bool, // whether to orient horizontally or vertically
+  showMonthLabels: PropTypes.bool, // whether to show month labels
+  monthLabelsColor: PropTypes.string,
+  showOutOfRangeDays: PropTypes.bool, // whether to render squares for extra days in week after endDate, and before start date
+  tooltipDataAttrs: PropTypes.oneOfType([PropTypes.object, PropTypes.func]), // data attributes to add to square for setting 3rd party tooltips, e.g. { 'data-toggle': 'tooltip' } for bootstrap tooltips
+  titleForValue: PropTypes.func, // function which returns title text for value
+  classForValue: PropTypes.func, // function which returns html class for value
+  onPress: PropTypes.func, // callback function when a square is clicked
+  colorArray: PropTypes.array
+};
+
+CalendarHeatmap.defaultProps = {
+  numDays: 200,
+  endDate: new Date(),
+  gutterSize: 2,
+  horizontal: true,
+  showMonthLabels: true,
+  monthLabelsColor: 'black',
+  showOutOfRangeDays: false,
+  colorArray: rectColor,
+  classForValue: value => (value ? "black" : "#8cc665"),
+  onPress: () => console.log("change onPress prop")
+};
+
+export default CalendarHeatmap;
